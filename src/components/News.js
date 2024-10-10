673bcfbbccadffde5d5d59ab8cf30c593e2db761
Mehtab <150262610+Mehtab-786@users.noreply.github.com>
2024-10-10 06:47:07 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -10,26 +10,25 @@ function useNewzHeadlines() {
     const [loading, setLoading] = useState(true);  // Track loading state
     
     useEffect(() => {
+        setLoading(true)
         fetch( `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
         .then(res => res.json())
         .then((res) => {
+          
             if (res.status === "ok") {
               setData(res.articles);  // Set the articles array in state
+              setLoading(false); // Set loading to false after response
               setError(null);  // Clear any previous errors
             } else {
-                setError("Failed to fetch news");
+              setError("Failed to fetch news");
             }
-              setLoading(false); // Set loading to false after response
-            })
+          })
             .catch((err) => {
               setError("An error occurred while fetching news"); // Handle network or other errors
               setLoading(false); // Stop loading on error
             });
     },[])
-
     return { data, error, loading }; // Return all states
 
-
 }
-
 export default useNewzHeadlines
